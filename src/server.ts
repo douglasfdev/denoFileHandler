a8f9bb662a7663d7bb7cc906092ce6b075639788
Douglas Fernandes Pereira Jr <douglas.fernandes91@live.com>
2023-11-03 20:33:01 -0300

    fix(server): remove wrong string connection

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import app from "./main.ts";
 
 app.listen({ port: 8080 });
 app.addEventListener("listen", ({ hostname, port, secure }) => {
-  log.info(`Database: ${env.DBMYSQL_DATABASE_NAME}`);
+  log.info(`Database: ${env.MYSQL_DATABASE_NAME}`);
   log.success(`Server is listening on ${secure ? "https": "http"}://${hostname}:${port}`);
   log.success(`Server is running on ${hostname}:${port}`)
 })
